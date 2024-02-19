68177308283df87ac2d7401cc007c0e5fb2d9f49
yerim.ju <yerim.ju@lge.com>
2024-02-19 21:14:34 +0900

    initial commit for iddd

---
+++ run.sh
@@ -0,0 +1,18 @@
+python finetune.py \
+    --base_model 'meta-llama/Llama-2-13b-hf' \
+    --data_path './alpaca_data_gpt4_700.json' \
+    --output_dir './lora-alpaca' \
+    --batch_size 16 \
+    --micro_batch_size 2 \
+    --num_epochs 3 \
+    --learning_rate 1e-4 \
+    --cutoff_len 512 \
+    --val_set_size 200 \
+    --lora_r 8 \
+    --lora_alpha 16 \
+    --lora_dropout 0.05 \
+    --lora_target_modules '[q_proj,v_proj]' \
+    --train_on_inputs \
+    --group_by_length \
+    --wandb_project 'id-data-descriptor-llm' \
+    --wandb_run_name 'gpt3.5-sft-lora'
