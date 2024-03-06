6ee77b39ecf70567c150eaa0e05080edd9d7bfce
yerim.ju <yerim.ju@lge.com>
2024-03-06 12:00:08 +0900

    add inference

--- inference.py
+++ inference.py
@@ -1,13 +1,20 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
 model_id = "meta-llama/Llama-2-13b-hf"
-local_model = "./outputs/loha"
+local_model = "./outputs/lora"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-model = AutoModelForCausalLM.from_pretrained(local_model)
+# model = AutoModelForCausalLM.from_pretrained(
+#     local_model, 
+#     load_in_8bit=True,
+#     torch_dtype=torch.float16,
+#     device_map='auto'
+# )
+model = AutoModelForCausalLM.from_pretrained(model_id)
 
-text = "Hello my name is"
+text = "What is the capital of France?"
 inputs = tokenizer(text, return_tensors="pt")
 
-outputs = model.generate(**inputs, max_new_tokens=20)
+outputs = model.generate(**inputs, max_new_tokens=50)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
